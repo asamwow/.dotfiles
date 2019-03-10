039fa67b8d053ed2e632fd90feff61287c212d58
asamwow <asamwow@github.com>
2019-03-10 13:26:47 -0700

    initial commit

---
+++ bin/bin/remove-hearthstone-location-plugin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm .wine/drive_c/Program\ Files\ \(x86\)/Hearthstone/Hearthstone_Data/Plugins/LocationAPI.dll
---
+++ emacs/.doom.d/packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(package! speed-type)
+(package! org-journal)
+(package! notmuch)
+(package! eclim)
