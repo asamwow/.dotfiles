83f20d19b5c022679ccc5c080b5d7f5b6fd3083e
Samuel Jahnke <asamwow@github.com>
2020-01-24 07:14:13 -0500

    fix and format

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -124,12 +124,14 @@
 (defconst my-mode-line-map (make-sparse-keymap))
 (defun notmuch-unread-count ()
   (setq notmuch-unread-email-count (string-to-number(replace-regexp-in-string "\n" "" (notmuch-command-to-string "count" "tag:inbox"))))
-  (if (eq notmuch-unread-email-count 0) (setq notmuch-unread-mode-line-string (format "  %d" (string-to-number(replace-regexp-in-string "\n" "" (notmuch-command-to-string "count"))))) (setq notmuch-unread-mode-line-string (format "  %d" inboxCount)))
+  (if (eq notmuch-unread-email-count 0)
+      (setq notmuch-unread-mode-line-string (format "  %d" (string-to-number(replace-regexp-in-string "\n" "" (notmuch-command-to-string "count")))))
+      (setq notmuch-unread-mode-line-string (format "  %d" notmuch-unread-email-count)))
   (force-mode-line-update))
-(run-at-time nil 10 'notmuch-unread-count)
+(run-at-time nil 5 'notmuch-unread-count)
 (defun notmuch-open-emails ()
   (interactive)
-  (if (eq notmuch-unread-email-count 0) (notmuch-search "*") (notmuch-search "tag:deleted")))
+  (if (eq notmuch-unread-email-count 0) (notmuch-search "*") (notmuch-search "tag:inbox")))
 (setq global-mode-string 
       (append global-mode-string (list '(:eval (propertize notmuch-unread-mode-line-string 'help-echo "notmuch emails" 'mouse-face 'mode-line-highlight 'local-map my-mode-line-map)))))
  (define-key my-mode-line-map 
